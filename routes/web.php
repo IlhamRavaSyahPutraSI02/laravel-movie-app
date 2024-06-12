55ce643043727138cd6b3ae07b43f9bb008a63f6
IlhamRavaSyahPutraSI02 <elravasyah@gmail.com>
2024-06-12 15:43:42 +0700

    upload praktikum 13

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,23 +13,20 @@ Route::get('/', function () {
 
 Route::get('/products', [ProductController::class, 'index']);
 
-Route::resource('/movies', MovieController::class);
-Route::resource('/genres', GenreController::class);
-Route::resource('/reviews', ReviewController::class);
 
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::resource('/movies', MovieController::class);
+    Route::resource('/genres', GenreController::class);
+    Route::resource('/reviews', ReviewController::class);
 
+});
 
-// Route::get('/reviews', [ReviewController::class, 'index']);
-// Route::get('/reviews/create', [ReviewController::class, 'create']);
-// Route::post('/reviews', [ReviewController::class, 'store']);
-// Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
-// Route::get('/reviews/{review}/edit', [ReviewController::class, 'edit']);
-
-
-
-
-// Route::get('/si02', function () {
-//     return "belajar laravel";
-// });
+require __DIR__.'/auth.php';
